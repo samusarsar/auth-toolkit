b6cf2990899e5a60d075b554f65f2cf420354b7a
samu.sar <samuil.sarandev@gmail.com>
2024-01-07 14:16:58 +0200

    Add: add initial Home page and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,42 @@
-export default function Home() {
-	return <div>Auth Toolkit</div>;
-}
+import { Poppins } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import LoginButton from '@/components/auth/login-button';
+
+const font = Poppins({
+	subsets: ['latin'],
+	weight: ['600'],
+});
+
+const Home = async () => {
+	return (
+		<main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+			<div className='space-y-6 text-center'>
+				<h1
+					className={cn(
+						'text-6xl font-semibold text-white drop-shadow-md',
+						font.className
+					)}
+				>
+					🔐 Auth
+				</h1>
+				<p className='text-white'>
+					A comprehensive authentication toolkit.
+				</p>
+				<div>
+					<LoginButton>
+						<Button
+							variant='secondary'
+							size='lg'
+						>
+							Sign in
+						</Button>
+					</LoginButton>
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default Home;
