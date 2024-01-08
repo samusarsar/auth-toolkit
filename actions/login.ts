67c5c6a01c31c1dcc55ea0bd36bd9426674001b8
samu.sar <samuil.sarandev@gmail.com>
2024-01-08 17:08:05 +0200

    Add: extend login action to log user in

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 'use server';
 
 import * as z from 'zod';
+import { AuthError } from 'next-auth';
 
+import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 	const validatedFields = LoginSchema.safeParse(values);
@@ -14,8 +17,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 		};
 	}
 
-	return {
-		error: false,
-		message: 'User logged in!',
-	};
+	const { email, password } = validatedFields.data;
+
+	try {
+		await signIn('credentials', {
+			email,
+			password,
+			redirectTo: DEFAULT_LOGIN_REDIRECT,
+		});
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return { error: true, message: 'Invalid credentials!' };
+				default:
+					return { error: true, message: 'Something went wrong!' };
+			}
+		}
+
+		throw error;
+	}
 };
