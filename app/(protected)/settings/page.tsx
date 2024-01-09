a6359ba29c91152be0dce48e7a2c899638504232
samu.sar <samuil.sarandev@gmail.com>
2024-01-09 10:42:07 +0200

    Add: add custom callbacks to NextAuth

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,8 +1,12 @@
 import { logout } from '@/actions/logout';
+import { auth } from '@/auth';
 
 const SettingsPage = async () => {
+	const session = await auth();
+
 	return (
 		<div>
+			{JSON.stringify(session)}
 			<form action={logout}>
 				<button type='submit'>Sign out</button>
 			</form>
