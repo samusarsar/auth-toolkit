62b826a5efd5dbaba1ecd61e1dd69090e0f9c2b0
samu.sar <samuil.sarandev@gmail.com>
2024-01-10 11:54:41 +0200

    Add: add example client component to show user info

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { UserInfo } from '@/components/user-info';
+
+const ServerPage = () => {
+	const user = useCurrentUser();
+
+	return (
+		<UserInfo
+			label='📱 Client component'
+			user={user}
+		/>
+	);
+};
+
+export default ServerPage;
